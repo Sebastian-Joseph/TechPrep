101f56464e159d7ca51541807d4cd407a5a4fcc7
Sebastian-Joseph <theclimax25@gmail.com>
2024-12-04 22:54:55 -0500

    Added LinkedIn and Career Guide

--- app/src/main/java/com/kean/edu/techprep/Careers.java
+++ app/src/main/java/com/kean/edu/techprep/Careers.java
@@ -1,13 +1,59 @@
 package com.kean.edu.techprep;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Careers extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_careers);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_home)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_resume)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_career)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_linkedin)));
+        TabLayout.Tab careerTab = tabLayout.getTabAt(2);
+        if (careerTab != null) {
+            careerTab.select();
+        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        startActivity(new Intent(Careers.this, MainActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(Careers.this, Resume.class));
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        startActivity(new Intent(Careers.this, LinkedIn.class));
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
 }
--- app/src/main/java/com/kean/edu/techprep/LinkedIn.java
+++ app/src/main/java/com/kean/edu/techprep/LinkedIn.java
@@ -1,13 +1,60 @@
 package com.kean.edu.techprep;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class LinkedIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_careers);
+        setContentView(R.layout.activity_linked_in);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_home)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_resume)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_career)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.tab_linkedin)));
+        TabLayout.Tab linkedInTab = tabLayout.getTabAt(3);
+        if (linkedInTab != null) {
+            linkedInTab.select();
+        }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        startActivity(new Intent(LinkedIn.this, MainActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(LinkedIn.this, Resume.class));
+                        break;
+                    case 2:
+                        startActivity(new Intent(LinkedIn.this, Careers.class));
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
 }
